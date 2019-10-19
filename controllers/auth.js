5be35044b44a2d6da3ad95e08146ec192a22bf24
romilshah525 <romilshah525@users.noreply.github.com>
2019-10-19 19:50:41 +0530

    Flash Failed!

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,11 +9,11 @@ const User = require('../models/user');
 const App = require('../app');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'shahromil525@gmail.com',
-        pass: 'rldegzyqjsqirwpf'
-    }   
+	service: 'gmail',
+	auth: {
+		user: 'shahromil525@gmail.com',
+		pass: 'rldegzyqjsqirwpf'
+	}   
 });
 
 passport.use(new localStrategy(User.authenticate()));
@@ -21,73 +21,82 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 exports.getSignUp = (req, res) => {
-    res.render('auth/signup');
+	if (req.flash('error').length > 0) {
+		console.log(req.flash('error'));
+	}
+	res.render('auth/signup');
 };
 
 exports.postSignUp = (req, res) => {
-    req.body.username
-    req.body.name
-    req.body.gender
-    req.body.address
-    req.body.age
-    req.body.contact
-    const email = req.body.email
-    User.register( new User({
-        username: req.body.username,
-        name: req.body.name,
-        gender: req.body.gender,
-        address: req.body.address,
-        age: req.body.age,
-        contact: req.body.contact,
-         }),
-        req.body.password,
-        function(err, user) {
-            if(err) {
-                req.flash('error','Enter valid credentials!');
-                console.log(`Error:${err}`);
-                return res.redirect('/signup');
-            }
-            console.log(`User Saved Details:${user}`);
-            passport.authenticate('local')(req, res, function(){
-                console.log(`Sending mail to : ${user.username}...\n`);
-                let mailOptions = {
-                    from: 'shahromil525@gmail.com',
-                    to: user.username,
-                    subject: 'Email Registered on ABC Pharmacy!',
-                    text: `Hey there, your account has been created!\n\nYour Username is ${user.username}`
-                };
-                transporter.sendMail(mailOptions, (err, info) => {
-                    if (err) {
-                        console.log(`Email Not Send!\n`);
-                        console.log(err);
-                    } else {
-                        console.log(`Email sent successfully: ${info.response}`);
-                    }
-                    res.redirect('/login');
-                });
-                    res.redirect('/login');
-            });
-        }
-    );
+	req.body.username
+	req.body.name
+	req.body.gender
+	req.body.address
+	req.body.age
+	req.body.contact
+	const email = req.body.email
+	User.register( new User({
+		username: req.body.username,
+		name: req.body.name,
+		gender: req.body.gender,
+		address: req.body.address,
+		age: req.body.age,
+		contact: req.body.contact,
+		 }),
+		req.body.password,
+		function(err, user) {
+			if(err) {
+				req.flash('error','Enter valid credentials!');
+				console.log(`Error:${err}`);
+				return res.redirect('/signup');
+			}
+			console.log(`User Saved Details:${user}`);
+			passport.authenticate('local')(req, res, function(){
+				console.log(`Sending mail to : ${user.username}...\n`);
+				let mailOptions = {
+					from: 'shahromil525@gmail.com',
+					to: user.username,
+					subject: 'Email Registered on ABC Pharmacy!',
+					text: `Hey there, your account has been created!\n\nYour Username is ${user.username}`
+				};
+				transporter.sendMail(mailOptions, (err, info) => {
+					if (err) {
+						console.log(`Email Not Send!\n`);
+						console.log(err);
+					} else {
+						console.log(`Email sent successfully: ${info.response}`);
+					}
+					res.redirect('/login');
+				});
+					res.redirect('/login');
+			});
+		}
+	);
 };
 
 exports.getLogin = (req, res) => {
-    res.render("auth/login");
+	console.log(`----->ERROR:${req.flash('error')}`);
+	console.log(`----->failureFlash:${req.flash('failureFlash')}`);
+	console.log(req);
+	
+	res.render("auth/login");
 };
 
 exports.postLogin = (req, res) => {
-    passport.authenticate('local', {
-        successRedirect: '/medicine-list',
-        failureRedirect: '/login',
-        failureFlash: 'Invalid Username or Password!',
-    }), function(req, res) {
-        res.redirect('/medicine-list');
-    }
+	console.log(`REQUEST:${req}`);
+	passport.authenticate('local', {
+		successRedirect: '/medicine-list',
+		failureRedirect: '/login',
+		failureFlash: 'Invalid Username or Password!'
+		// failureFlash: true
+	}), function(req, res) {
+		res.redirect('/medicine-list');
+	}
 }
 
 exports.getLogout = (req, res) => {
-    req.logout();
-    res.redirect('/');
+	req.logout();
+	res.redirect('/');
 };
 
 // exports.getResetPassword = (req, res) => {
